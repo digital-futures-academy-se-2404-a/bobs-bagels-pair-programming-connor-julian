7c4ac86af13b30d74a49e9b8bb237a32cad2bdfc
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-04-18 17:34:03 +0100

    NEW: User Stories 1 & 2 test cases done and passed

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -9,6 +9,7 @@ const cleanUp = () => {
     actual = undefined;
     result = undefined;
     item = undefined;
+    item2 = undefined;
     basket.basketItems = [];
 }
 
@@ -17,9 +18,10 @@ console.log("Tests for User Story 1:")
 console.log("==================================")
 
 
-console.log("Test 1: Add 1 item to a basket using addItem")
+console.log("Test 1: Does the length of basketItems increase by 1")
 //  Arrange
 let item = {};
+let item2 = {};
 let expected = basket.basketItems.length + 1;
 let actual, result;
 //  Act
@@ -34,6 +36,183 @@ console.log("==================================")
 // Clean Variables
 cleanUp();
 
+// Test 2
+console.log("Test 2: Does basketItems contain the new item added by addItem()")
 
-console.log("Test 2: Check that item added using addItem() was the item to be added")
+// Arrange
+item = {};
+expected = true;
+// Act
+basket.addItem(item);
+actual = basket.basketItems.includes(item);
+// Assert
+result = assertEquals(actual, expected);
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
 
+
+
+// Test 3
+console.log("Test 3: Can you add to a basket already containing items using addItem()")
+
+// Arrange
+item = {};
+item2 = {};
+expected = true;
+// Act
+basket.addItem(item2);
+basket.addItem(item);
+actual = basket.basketItems.includes(item);
+// Assert
+result = assertEquals(actual, expected);
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
+
+
+
+// Test 4
+console.log("Test 4: Test that the basket doesn't accept a null item")
+// Arrange
+item = null;
+expected = basket.basketItems.length;
+
+// Act
+basket.addItem(item);
+actual = basket.basketItems.length;
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
+
+
+// * User Story 2
+console.log("==================================");
+console.log("User Story 2 Tests");
+console.log("==================================");
+
+// Test 1
+console.log("Test 1: Removing an item decreases the basket contents by 1")
+// Arrange
+item = {};
+basket.addItem(item);
+expected = basket.basketItems.length - 1;
+// Act
+basket.removeItem(item);
+actual = basket.basketItems.length;
+// Assert
+result = assertEquals(actual, expected);
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
+
+// Test 2
+console.log("Test 2: Check that If multiples items are in the basket, only the item to be removed is removed");
+
+// Arrange
+item = {};
+basket.addItem(item);
+item2 = "bagel";
+basket.addItem(item2);
+expected = false;
+
+// Act
+basket.removeItem(item);
+actual = basket.basketItems.includes(item);
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
+
+
+
+// Test 3
+console.log("Test 3: If the basket contains multiples of the same item type, only 1 is removed");
+
+// Arrange
+item = {};
+basket.addItem(item);
+basket.addItem(item);
+expected = basket.basketItems.length - 1;
+
+// Act
+basket.removeItem(item);
+actual = basket.basketItems.length;
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
+
+
+// Test 4
+console.log("Test 4: Check that we can't remove an item the basket doesn't contain");
+// Arrange
+item = {};
+item2 = "bagel";
+basket.addItem(item2);
+expected = basket.basketItems.length;
+
+// Act
+basket.removeItem(item);
+actual = basket.basketItems.length;
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
+
+
+// Test 4
+console.log("Test 5: Check that we can't remove a Null item");
+// Arrange
+item = null;
+item2 = "bagel";
+basket.addItem(item2);
+expected = basket.basketItems.length;
+
+// Act
+basket.removeItem(item);
+actual = basket.basketItems.length;
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log("==================================");
+// Clean Variables
+cleanUp();
--- src/basket.js
+++ src/basket.js
@@ -2,10 +2,14 @@
 * @type { Object }
 */
 const basket = {
-    addItem: function (item) {
-        this.basketItems.push(item);
-    },
     basketItems: [],
+    addItem: function (item) {
+        (item !== null) && this.basketItems.push(item);
+    },
+    removeItem: function (item) {
+        let indexForRemoval = this.basketItems.indexOf(item);
+        (indexForRemoval !== -1) && this.basketItems.splice(indexForRemoval, 1);
+    }
 };
 
 export default basket;
